6595c7e62bb02d5ed4e23c8d4800dfcdcafd1c6b
Mihir Kumar Shrestha <121042506+Mihirr42@users.noreply.github.com>
2023-12-23 13:02:17 +0530

    Square1.cpp

---
+++ WhileLoop/Pattern Questions/Square1.cpp
@@ -0,0 +1,33 @@
+7777777
+7777777
+7777777
+7777777
+7777777
+7777777
+7777777
+
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+	int n;
+	cin>>n;
+
+	int row=1;
+
+	while(row<=n){
+
+		int col=1;
+		while(col<=n){
+			cout<<n;
+			col++;
+		}
+
+		cout<<endl;
+		row++;
+	}
+}
+
+// MIHIRR42
